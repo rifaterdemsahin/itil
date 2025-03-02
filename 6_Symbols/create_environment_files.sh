046e2ac485a5d77ddf5a33c5bcd796f99b9a87d7
Erdem <rifaterdemsahin@gmail.com>
2025-03-02 18:43:50 +0000

    Add initial files and structure for project objectives, journeys, environments, and formulas

---
+++ 6_Symbols/create_environment_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# chmod +x /workspaces/itil/create_environment_files.sh;/workspaces/itil/create_environment_files.sh
+
+touch 3.1.1_environment_XXX.md
+echo "environment: XXX Prompt: Write Pros and Cons" >> 1.1.1_objective_XXX.md
+touch 3.2.1_environment_XXX.md
+echo "environment: XXX Prompt: Write Pros and Cons" >> 1.2.1_objective_XXX.md
+touch 3.3.1_environment_XXX.md
+echo "environment: XXX Prompt: Write Pros and Cons" >> 1.3.1_objective_XXX.md
+
---
+++ 6_Symbols/create_real_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# chmod +x ./create_real_files.sh;./create_real_files.sh
+
+touch 1.1.1_objective_XXX.md
+echo "Objective:" >> 1.1.1_objective_XXX.md
+touch 1.2.1_objective_XXX.md
+echo "Objective:" >> 1.2.1_objective_XXX.md
+touch 1.3.1_objective_XXX.md
+echo "Objective:" >> 1.3.1_objective_XXX.md
+
